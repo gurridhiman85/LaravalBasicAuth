2ca79e962243c7eb97a28e36ef4b1bb0310fd88b
Gurri <gurri.dhiman95@gmail.com>
2022-02-24 18:25:20 +0530

    Add created at descending order

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,12 +9,12 @@ use Spatie\Activitylog\Models\Activity;
 class AdminController extends Controller
 {
     public function allusers(){
-        $users = User::get();
+        $users = User::orderByDesc('created_at')->get();
         return view('admin.users',compact('users'));
     }
 
     public function allactivities(){
-        $activities = Activity::with(['user'])->get();
+        $activities = Activity::with(['user'])->orderByDesc('created_at')->get();
         return view('admin.activities',compact('activities'));
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,5 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class
         ]);
-    }    
+    }
 }
